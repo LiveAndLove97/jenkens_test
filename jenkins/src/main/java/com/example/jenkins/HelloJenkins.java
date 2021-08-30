4437af7014d155159effe37b5111bf64069be084
Guojl1997 <1023971996@qq.com>
2021-08-30 15:14:55 +0800

    test push

---
+++ jenkins/src/main/java/com/example/jenkins/HelloJenkins.java
@@ -0,0 +1,12 @@
+package com.example.jenkins;
+
+/**
+ * @ClassName HelloJenkins
+ * @Description TODO
+ * @Author 暗影
+ * @Date 2021/8/30 15:12
+ * @Version 1.0
+ **/
+public class HelloJenkins {
+    int i = 1;
+}
